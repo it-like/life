2174d489e8e839d0b3c6ad744d50efc60dd592ca
Gille <93673070+it-like@users.noreply.github.com>
2024-06-14 22:55:21 +0200

    Add css templates for flashcards, works interactively good for now. Cards need better structure, folder hierarchy idealy.

--- flashcards/CreateCards.js
+++ flashcards/CreateCards.js
@@ -1,58 +1,50 @@
+class CreateCards {
 
-
-
-
-// Class for creating deck with cards, .txt-> .JSON
-class CreateCards{
-
-
-  constructor(path){
-    this.TxtPath = 'flashcards/decks/TXTFiles/' + path + '.txt';
-    this.jsonPath = 'flashcards/decks/JSONFiles/' + path + '.json';
-    this.createCards();
+  constructor(path) {
+      this.TxtPath = 'flashcards/decks/TXTFiles/' + path + '.txt';
+      this.jsonPath = 'flashcards/decks/JSONFiles/' + path + '.json';
+      this.createCards();
   }
 
-  // Cards have format [deck, front, back, tags]
-  createCards(){
-    const fs = require('fs');
-    fs.readFile(this.TxtPath, 'utf-8', (err, data) =>{
-      if (err){
-          console.error(err);
-          return;
-      }
-
-      let rows = data.split('\n'); // row ≡ card
-      let cards = [];
-      rows.forEach((row) => {
-          if (row.trim()) { // false if empty string ('')
-            let parts = row.split('\t'); //  seperate at tabs
-            if (parts.length == 4) { 
-              let card = {
-                category: parts[0].trim(),
-                term: parts[1].trim(),
-                definition: parts[2].trim(),
-                tags:  parts[3].trim(),
-              };
-              cards.push(card);
-            } else {
-              console.error('Card format not \'[deck, front, back, tags]\'. Skipping', row);
-            }
-          }
-        });
-      //console.log(cards);
-      // Create JSON of data, replaces file if path already exists for file
-      fs.writeFile(this.jsonPath, JSON.stringify(cards, null, 2), (err) => {
+  createCards() {
+      const fs = require('fs');
+      fs.readFile(this.TxtPath, 'utf-8', (err, data) => {
           if (err) {
-            console.error(err);
-          } else {
-            console.log('Data has been converted to JSON');
+              console.error(err);
+              return;
           }
-        });
-  })
+
+          let rows = data.split('\n');
+          let cards = [];
+          rows.forEach((row) => {
+              if (row.trim()) {
+                  let parts = row.split('\t');
+                  if (parts.length == 4) {
+                      let card = {
+                          category: (parts[0]),
+                          term: (parts[1]),
+                          definition: (parts[2]),
+                          tags: (parts[3]),
+                      };
+                      cards.push(card);
+                  } else {
+                      console.error('Card format not \'[deck, front, back, tags]\'. Skipping', row);
+                  }
+              }
+          });
+
+          fs.writeFile(this.jsonPath, JSON.stringify(cards, null, 2), (err) => {
+              if (err) {
+                  console.error(err);
+              } else {
+                  console.log('Data has been converted to JSON');
+              }
+          });
+      });
   }
-};
 
+}
 
-new CreateCards('geometrical_vectors');
+new CreateCards('Geometrical_Vectors');
 new CreateCards('Quotes');
-
+new CreateCards('Matrices');

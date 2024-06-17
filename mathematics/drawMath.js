5b1895fe26c184393cad6e9d7578a26ed06a4325
Gille <93673070+it-like@users.noreply.github.com>
2024-06-17 22:07:44 +0200

    Add fancier math (rm border, wider), flashcards work.

--- mathematics/drawMath.js
+++ mathematics/drawMath.js
@@ -1,48 +1,43 @@
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext('2d');
+
+canvas.style.border = 'none';
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight/2;
+
 var canvasCenterX = canvas.width/2;
 var canvasCenterY= canvas.height/2;
-
-function calculateXValues(){
-    for (let i = 0;i < 200; i++ ) {
-        setTimeout(() => {
-                console.log(i);
-        }, i*10);
-    }
-   
-}
 
 function plotSine(ctx, x=0){
     var width = ctx.canvas.width;
     var height = ctx.canvas.height;
 
-
     var amplitude = 100;
-    var frequency = Math.PI*10;
+    var frequency = ctx.canvas.width/(Math.PI*22);
     //var period = 2 * Math.PI * 40 ; // P = 2pi/frequency
-    
     
     if (x>=width){
         // ctx.clearRect(0, 0, canvas.width, canvas.height); // reset canvas
-        // x = 0
+        //  x = 0;
         return;
     };
-    var y = height/2 + amplitude*Math.sin(x/frequency);
+
+    var y = height/2 + amplitude*Math.sin(x/frequency + frequency/2 );
 
     if (x===0){
         ctx.beginPath();
         ctx.moveTo(x,y);
-    } else{
-        ctx.lineTo(x,y)
     }
+
+    ctx.lineTo(x,y)
     ctx.stroke();
-    ctx.lineTo(canvasCenterX,canvasCenterY);
+    ctx.lineTo(canvasCenterX, canvasCenterY*2);
     ctx.stroke();
     setTimeout(() => plotSine(ctx, x = x+2),1);
 }
 
-function unitCircle (ctx,xPos, yPos, timeout, phi = 0 ){
+function unitCircle (ctx,xPos, yPos, timeout, phi = Math.PI ){
     var radius = 1;
     xPos += radius * Math.cos(phi);
     yPos += radius * Math.sin(phi);
@@ -66,12 +61,23 @@ if (phi ===0){
         {setTimeout(() => unitCircle(ctx, xPos, yPos, true, phi = phi+0.01),1)}
     else{
         unitCircle(ctx, xPos, yPos, false, phi = phi+0.01)}
-    
-    
+     
+}
+
+function calculateXValues(){
+    for (let i = 0;i < 200; i++ ) {
+        setTimeout(() => {
+                console.log(i);
+        }, i*10);
+    }
+   
 }
 
 
 plotSine(ctx)
+
+
+
 //unitCircle(ctx,canvasCenterX, canvasCenterY, true, 0 );
 
 //calculateXValues()

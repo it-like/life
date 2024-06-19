59c85c8bc914d4ed7ee01261e75091b8aef50fd2
Gille <93673070+it-like@users.noreply.github.com>
2024-06-19 10:39:02 +0200

    Add first implementation of p5.js

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,32 @@
+//global variables
+let red = true;
+let counter =0;
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+  angleMode(DEGREES);
+} 
+setup();
+function draw() {
+  noFill();
+  background(0,200,145);
+  //rotateX(90 +counter);
+  speedOfRotation = sin(counter/10)*180;
+  rotateY(speedOfRotation);
+  rotateX(speedOfRotation);
+  rotateZ(speedOfRotation);
+  for(var i = 0; i < 360; i+=20){
+    beginShape();
+    for(var j = 0; j < 360; j+=5){
+      var rad = i;
+      let x = rad * cos(j);
+      let y = rad * sin(j);
+      let z = 20*sin(frameCount*8 + i/2) ;
+      counter += 0.001;
+      vertex(x , y, z);
+    }
+    endShape(CLOSE)
+  }
+
+}
+  
+

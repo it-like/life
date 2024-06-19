7ee5c37d06a7375654c4a1943b54e136dd6b8b7f
Gille <93673070+it-like@users.noreply.github.com>
2024-06-19 14:20:59 +0200

    Add unit-circle w. corresponding sin & cos

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,28 +1,73 @@
 //global variables
-let red = true;
+phi = 0;
 let counter =0;
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight, WEBGL);
   angleMode(DEGREES);
+  frameRate(120); 
+
 } 
-setup();
+
 function draw() {
+   //wobblingWave();
+  stiroGraph();
+  
+}
+
+
+function stiroGraph() {
+  background('orange'); 
+  const diameter =200;
+  radius = diameter/2;
+  translate(-window.innerWidth/3,window.innerWidth/7)
+  //rotateX(phi);
+  noFill();
+  stroke(0);
+  ellipse(0, 0,  diameter, diameter); 
+    
+  let x = radius * cos(phi);
+  let y = radius * sin(phi);
+  
+  // rotate blue ball
+  
+  translate(x,y);   
+  ellipse(0, 0, 4, 4); 
+  translate(-x,-y);
+  // rotate back
+
+
+ 
+  line(-radius,0,radius,0) // x-axis
+  line(0,-radius,0,radius) // y-axis
+
+  strokeWeight(4);
+  line(0,0,x,y) // hypothenuse
+  line(0,0, x,0) // cos
+  line(x,0,x,y) // sin
+  strokeWeight(1);
+  phi -= 1
+
+
+  
+}
+  
+
+function wobblingWave(){
   noFill();
   background(0,200,145);
   //rotateX(90 +counter);
-  speedOfRotation = sin(counter/20)*180;
-  rotateY(speedOfRotation);
-  rotateX(speedOfRotation);
-  rotateZ(speedOfRotation);
+  speedOfRotation = sin(counter/20)*100;
+  rotateX(75);
   stroke(255,165,0);
   strokeWeight(5);
-  for(var i = 0; i < 360; i+=20){
+  for(var i = 0; i < 360; i+=10){
     beginShape();
     for(var j = 0; j < 360; j+=5){
-      var rad = i;
+      var rad = i/1.5;
       let x = rad * cos(j);
       let y = rad * sin(j);
-      let z = 20*sin(frameCount*8 + i/2) ;
+      let z = 20*sin(frameCount*4 + i/2) ;
       counter += 0.001;
       vertex(x , y, z);
     }
@@ -30,5 +75,3 @@ function draw() {
   }
 
 }
-  
-
